c25b96b5fa9ef502c5104645303111ab49e26021
rejahmuhammed <rejahmuhammed@gmail.com>
2025-03-06 01:27:25 -0500

    Add files via upload

---
+++ MARCH 2025/WEEK 1/PrimeNumbers50to100/PrimeNumbers50to100.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Prime numbers between 50 and 100 are:"
+
+for ((num=50; num<=100; num++)); do
+    is_prime=1
+
+    for ((i=2; i*i<=num; i++)); do
+        if ((num % i == 0)); then
+            is_prime=0
+            break
+        fi
+    done
+
+    if ((is_prime == 1)); then
+        echo -n "$num "
+    fi
+done
+
+echo ""
