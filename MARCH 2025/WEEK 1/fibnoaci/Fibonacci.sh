b28739c0b1078661ea74b193951e8565227530b8
rejahmuhammed <rejahmuhammed@gmail.com>
2025-03-06 01:15:05 -0500

    Add files via upload
    code

---
+++ MARCH 2025/WEEK 1/fibnoaci/Fibonacci.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p "Enter the number of terms: " n
+
+a=0
+b=1
+
+echo "Fibonacci series:"
+for ((i=0; i<n; i++)); do
+    echo -n "$a "
+    fn=$((a + b))
+    a=$b
+    b=$fn
+done
+
+echo ""
+
+
