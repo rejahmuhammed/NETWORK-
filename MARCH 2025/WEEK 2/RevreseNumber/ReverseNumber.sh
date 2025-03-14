692629da569ef4f7aab08c4c69e16b213432af2d
rejahmuhammed <rejahmuhammed@gmail.com>
2025-03-14 01:02:53 -0400

    Add files via upload
    code

---
+++ MARCH 2025/WEEK 2/RevreseNumber/ReverseNumber.sh
@@ -0,0 +1,13 @@
+echo "Enter a 5-digit number:"
+read num
+
+rev=0
+while [ "$num" -gt 0 ];
+do
+    digit=$((num % 10))
+    rev=$((rev * 10 + digit))
+    num=$((num / 10))
+done
+
+echo "Reversed Number: $rev"
+
